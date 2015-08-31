c0e5cb9c2baaa4d3e9e612e8bc6ba27fe928b030
Dhananjay P <dpingale@tavisca.com>
2015-08-31 13:01:18 +0530

    anak images

--- js/ghosh-app.js
+++ js/ghosh-app.js
@@ -2,6 +2,13 @@ $(document).ready(function(){
 
 	$('.app-wrapper').addClass('animated flipInY');
 
+	$(".rippler").rippler({
+		effectClass      :  'rippler-effect'
+		,effectSize      :  16      // Default size (width & height)
+		,addElement      :  'svg'   // e.g. 'svg'(feature)
+		,duration        :  400
+	});
+
 	$("audio").on("play", function(){
 	    var _this = $(this);
 	    $("audio").each(function(i,el){
@@ -9,6 +16,7 @@ $(document).ready(function(){
 	            $(el).get(0).pause();
 	    });
 	});
+
 
 
 });
