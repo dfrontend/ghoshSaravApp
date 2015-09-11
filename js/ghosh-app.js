ca6f48a77a5129a3f9b89701d9018d94d83e5a72
Ravindra <ravindra@iternia.com>
2015-09-11 21:33:50 +0530

    play and pause added

--- js/ghosh-app.js
+++ js/ghosh-app.js
@@ -28,27 +28,97 @@ var app = {
 		//	//});
 		//});
 		$(document).ready(function() {
-			alert("loaded");
+			var media;
+			var media_button;
+			//alert("loaded");
 			function success() {
 				console.log("in success play audio");
 			}
 			function errorThrown(e) {
 				alert('Error while playing the sound!');
 			}
+
 			$("audio").on("play", function () {
+
+				if (media) {
+					media.pause();
+				}
 
 				var _this = $(this);
 				console.log(_this);
 //                function playAudio() {
                 var src = '/android_asset/www/anak/' + _this.attr("data-src");
 				//var src = _this.context.currentSrc;
-				var media = new Media(src, success, errorThrown);
-				alert(media);
+				media = new Media(src, success, errorThrown);
+				//alert(media);
 				media.play();
 				console.log(media);
 //                }
 //                alert(_this.context.currentSrc);
 			});
+
+
+
+			$(".media-play").on("click", function () {
+
+				if (media_button) {
+					media_button.pause();
+				}
+
+				var _this = $(this);
+				console.log(_this);
+//                function playAudio() {
+				var src = '/android_asset/www/anak/' + _this.attr("data-src");
+				//var src = _this.context.currentSrc;
+				media_button = new Media(src, success, errorThrown);
+				//alert(media);
+				media_button.play();
+				//console.log(media_button);
+//                }
+//                alert(_this.context.currentSrc);
+			});
+
+
+			$(".media-pause").on("click", function () {
+
+				if (media_button) {
+					media_button.pause();
+				}
+
+//				var _this = $(this);
+//				console.log(_this);
+////                function playAudio() {
+//				var src = '/android_asset/www/anak/' + _this.attr("data-src");
+//				//var src = _this.context.currentSrc;
+//				media_button = new Media(src, success, errorThrown);
+//				//alert(media);
+//				media_button.play();
+				//console.log(media_button);
+//                }
+//                alert(_this.context.currentSrc);
+			});
+
+
+
+			$("audio").on("pause", function () {
+				//alert("pause");
+				//var _this = $(this);
+				//console.log(_this);
+//                function playAudio() {
+//				var src = '/android_asset/www/anak/' + _this.attr("data-src");
+//				var src = _this.context.currentSrc;
+				//var media = new Media(src, success, errorThrown);
+				//alert(media);
+				if (media) {
+					media.pause();
+				}
+
+				//media.pause();
+				//console.log(media);
+                //}
+                //alert(_this.context.currentSrc);
+			});
+
 		});
 	}
 };
